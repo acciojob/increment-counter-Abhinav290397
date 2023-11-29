755c452b0a0fbbfdc9e699aefa56d6ecbec3c58e
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-29 16:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //your JS code here. If required.
-const Btn = document.quereySelector("incrementBtn");
+const P = document.getElementById("counter");
+const Btn = document.getElementById("incrementBtn");
+
+let count = 0;
+
+Btn.addEventListener('click', () => {
+alert(count);
+count++;
+P.textContent = count;
+});
+/*const Btn = document.quereySelector("incrementBtn");
 Btn.addEventListener("click",() =>{
 	const P = documnet.createElement("p");
 	P.innerText = 0;
 	P.className = "counter";
 	alert(P.innerText++);
-});
+});*/
